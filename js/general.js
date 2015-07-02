207bd986549584e31a82434e81082f75d15de461
elujambio <eugeniolujambio@gmail.com>
2015-07-02 18:43:21 -0500

    Menu Animation Working
    Added SVG spritesheet to menu burger

--- js/general.js
+++ js/general.js
@@ -4,6 +4,7 @@ var navbarMenu = document.getElementById("navbar-menu");
 navbarMenu.addEventListener("click", menuToggle);
 var menu = document.getElementById("menu");
 var toggle = 0;
+var spritesheet = document.getElementById("sprites");
 menu.style.opacity=0;
 menu.style.transition="opacity .3s";
 menu.style.transitionTimingFunction="ease-out";
@@ -15,10 +16,12 @@ function menuToggle() {
     if(toggle==0){
     	menu.style.opacity=0;
 		menu.style.pointerEvents="none";
+		sprites.style.animation="forward .2s steps(10) forwards"
 		toggle=1;
     }else{
     	menu.style.opacity=1;
 		menu.style.pointerEvents="all";    	
+		sprites.style.animation="backward .2s steps(10) forwards"
     	toggle=0;
     }
 }
