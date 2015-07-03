836ed2d1dbb7df47ad169d24f3624bf1f41a90e2
elujambio <eugeniolujambio@gmail.com>
2015-07-02 19:04:23 -0500

    Changed navbar units from em to px
    Fixes responsiveness issues

--- js/general.js
+++ js/general.js
@@ -3,12 +3,11 @@
 var navbarMenu = document.getElementById("navbar-menu");
 navbarMenu.addEventListener("click", menuToggle);
 var menu = document.getElementById("menu");
-var toggle = 0;
+var toggle = 1;
 var spritesheet = document.getElementById("sprites");
 menu.style.opacity=0;
 menu.style.transition="opacity .3s";
 menu.style.transitionTimingFunction="ease-out";
-menu.style.pointerEvents="all";
 
 
 function menuToggle() {
@@ -16,12 +15,12 @@ function menuToggle() {
     if(toggle==0){
     	menu.style.opacity=0;
 		menu.style.pointerEvents="none";
-		sprites.style.animation="forward .2s steps(10) forwards"
+		sprites.style.animation="backward .2s steps(10) forwards";
 		toggle=1;
     }else{
     	menu.style.opacity=1;
 		menu.style.pointerEvents="all";    	
-		sprites.style.animation="backward .2s steps(10) forwards"
+		sprites.style.animation="forward .2s steps(10) forwards";
     	toggle=0;
     }
 }
